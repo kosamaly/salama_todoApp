a44ab4d04c4e126237e65651be0a516bc0cd4472
Khaled Osama <khaledosama@Khaleds-MacBook-Air.local>
2023-05-29 03:13:37 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -7,43 +7,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: ' Todo state',
+      title: 'Todo State',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TodoList(),
+      home: ToDoList(),
     );
   }
 }
 
-class TodoList extends StatefulWidget {
-  const TodoList({super.key});
-
+class ToDoList extends StatefulWidget {
   @override
-  TodoListState createState() => TodoListState();
+  ToDoListState createState() => ToDoListState();
 }
 
-class TodoListState extends State<TodoList> {
+class ToDoListState extends State<ToDoList> {
   List<String> todos = [
-    // 'did you play footbal today ?',
-    // 'did you go to gym today?',
-    // 'did pay today?',
-    // 'what is you nam, i guess you are M.Salama fot sure, is not it?',
+    // 'Did you play football today?',
+    // 'Did you go to gym today?',
+    // 'Did pay today?',
+    // 'What is your nam, I guess you are M. Salama for sure, is not it?',
   ];
+
   void addTask(String task) {
     setState(() {
-      todos.add(myController.text);
-      todos.clear();
+      todos.add(task);
     });
   }
 
   final String varTitle = "";
   final myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo List'),
+        title: const Text('Todo List'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -51,7 +50,7 @@ class TodoListState extends State<TodoList> {
               context: context,
               builder: (BuildContext context) {
                 return Container(
-                  padding: EdgeInsets.all(18),
+                  padding: const EdgeInsets.all(18),
                   height: double.infinity,
                   color: Colors.white70,
                   child: Column(
@@ -60,9 +59,9 @@ class TodoListState extends State<TodoList> {
                       TextField(
                         controller: myController,
                         maxLength: 27,
-                        decoration: InputDecoration(labelText: " Add New Task"),
+                        decoration: InputDecoration(labelText: "Add new task"),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 12,
                       ),
                       TextButton(
@@ -71,7 +70,7 @@ class TodoListState extends State<TodoList> {
                             Navigator.pop(context);
                           },
                           child: Text(
-                            "ADD",
+                            "Add",
                             style: TextStyle(fontSize: 18),
                           )),
                     ],
@@ -79,28 +78,161 @@ class TodoListState extends State<TodoList> {
                 );
               });
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         backgroundColor: Colors.cyan,
       ),
       body: Container(
         color: Colors.cyan,
         width: double.infinity,
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          SizedBox(
+          const SizedBox(
             height: 80,
           ),
-          // ListView.builder(
-          //   itemCount: todos.length,
-          //   itemBuilder: (BuildContext context, int index) {
-          //     return Text("you Task: ${myController.text}");
-          //   },
-          // )
-          // Text(
-          //   "you Task: ${myController.text}",
-          //   style: TextStyle(fontSize: 22),
-          // )
+          Container(
+            color: Colors.yellow,
+            width: double.infinity,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Text(
+                  //   "Your Task: ${myController.text}",
+                  //   style: TextStyle(fontSize: 22),
+                  // ),
+                ]),
+          ),
+          Container(
+            width: double.infinity,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /// اقسم بالله مهم اللعب كله هنا
+                  ///
+                  ...todos
+                      .map((item) => Text(
+                            "$varTitle $item",
+                            style: const TextStyle(fontSize: 18),
+                          ))
+                      .toList()
+                ]),
+          ),
         ]),
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp(const MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: ' Todo state',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const TodoList(),
+//     );
+//   }
+// }
+//
+// class TodoList extends StatefulWidget {
+//   const TodoList({super.key});
+//
+//   @override
+//   TodoListState createState() => TodoListState();
+// }
+//
+// class TodoListState extends State<TodoList> {
+//   List<String> todos = [
+//     // 'did you play footbal today ?',
+//     // 'did you go to gym today?',
+//     // 'did pay today?',
+//     // 'what is you nam, i guess you are M.Salama fot sure, is not it?',
+//   ];
+//   void addTask(String task) {
+//     setState(() {
+//       todos.add(task);
+//     });
+//   }
+//
+//   final String varTitle = "";
+//   final myController = TextEditingController();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Todo List'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           showModalBottomSheet(
+//               context: context,
+//               builder: (BuildContext context) {
+//                 return Container(
+//                   padding: EdgeInsets.all(18),
+//                   height: double.infinity,
+//                   color: Colors.white70,
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       TextField(
+//                         controller: myController,
+//                         maxLength: 27,
+//                         decoration: InputDecoration(labelText: " Add New Task"),
+//                       ),
+//                       SizedBox(
+//                         height: 12,
+//                       ),
+//                       TextButton(
+//                           onPressed: () {
+//                             addTask(myController.text);
+//                             Navigator.pop(context);
+//                           },
+//                           child: Text(
+//                             "ADD",
+//                             style: TextStyle(fontSize: 18),
+//                           )),
+//                     ],
+//                   ),
+//                 );
+//               });
+//         },
+//         child: Icon(Icons.add),
+//         backgroundColor: Colors.cyan,
+//       ),
+//       body: Container(
+//         color: Colors.cyan,
+//         width: double.infinity,
+//         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+//           SizedBox(
+//             height: 80,
+//           ),
+//           Container(
+//             color: Colors.yellow,
+//             width: double.infinity,
+//             child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [
+//                   Text(
+//                     "your Task: ${myController.text}",
+//                     style: TextStyle(fontSize: 22),
+//                   ),
+//                 ]),
+//           ),
+//           Container(
+//             width: double.infinity,
+//             child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [...todos.map((item) => (varTitle))]),
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
